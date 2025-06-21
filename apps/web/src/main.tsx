a70ed8620b032d6494db4d9e9263fdc668f4849e
Karl Notturno <karl@karlnotturno.com>
2025-06-21 12:34:48 -0400

    HashRouter for Github pages.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import Admin from './Admin.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/admin" element={<Admin />} />
         <Route path="*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
